85a6fe66437273d5f73b498b4bc0f894561c7a52
BakaHideout <missgodthumbs@gmail.com>
2026-09-25 16:07:27 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'totoquest-v15';
+const CACHE_NAME = 'totoquest-v16';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png',
   './assets/loading-bg.jpg', './assets/girl-front.png', './assets/girl-back.png',
   './assets/boy-front.png', './assets/boy-back.png',
@@ -13,7 +13,17 @@ const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512
   './assets/totos/potion_slime.png', './assets/totos/sm/potion_slime.png',
   './assets/totos/moon_moth.png', './assets/totos/sm/moon_moth.png',
   './assets/totos/bone_bean.png', './assets/totos/sm/bone_bean.png',
-  './assets/totos/witchcap_sprout.png', './assets/totos/sm/witchcap_sprout.png'];
+  './assets/totos/witchcap_sprout.png', './assets/totos/sm/witchcap_sprout.png',
+  './assets/totos/shadow_reaper.png', './assets/totos/sm/shadow_reaper.png',
+  './assets/totos/frost_wyrm.png', './assets/totos/sm/frost_wyrm.png',
+  './assets/totos/pumpkin_colossus.png', './assets/totos/sm/pumpkin_colossus.png',
+  './assets/totos/nightmare_queen.png', './assets/totos/sm/nightmare_queen.png',
+  './assets/totos/blood_moon_wolf.png', './assets/totos/sm/blood_moon_wolf.png',
+  './assets/totos/thunder_serpent.png', './assets/totos/sm/thunder_serpent.png',
+  './assets/totos/toxic_basilisk.png', './assets/totos/sm/toxic_basilisk.png',
+  './assets/totos/soul_lantern.png', './assets/totos/sm/soul_lantern.png',
+  './assets/totos/obsidian_golem.png', './assets/totos/sm/obsidian_golem.png',
+  './assets/totos/hexen_drake.png', './assets/totos/sm/hexen_drake.png'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
